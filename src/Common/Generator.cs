db0b3b89e8276137997dcc877595ae40e0aa6fb9
TatuFlorin <sk.florin@yahoo.com>
2020-03-01 10:35:34 +0200

    Added id-generator.

---
+++ src/Common/Generator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public sealed class Generator
+    {
+        public static string GenerateId()
+        {
+            var id = new StringBuilder(Guid.NewGuid().ToString());
+
+            id.Replace("-", "");
+
+            return id.ToString();
+
+        }
+    }
+}
