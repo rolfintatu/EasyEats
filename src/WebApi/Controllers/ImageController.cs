5887c4f0e7c9742e33a0e131213e1a74e3524b8d
TatuFlorin <sk.florin@yahoo.com>
2020-04-10 17:24:01 +0300

    Added endpoints for product images.

---
+++ src/WebApi/Controllers/ImageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Common.Dtos;
+using Application.ProductImage.Commands;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Common.Extensions;
+using Application.ProductImage.Queries;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : BaseController
+    {
+        //Queries
+        [HttpGet]
+        public async Task<List<ImageDto>> GetByProductAsync(int productId)
+            => await mediator.Send(new ImagesByProduct(productId));
+
+        //Commands
+        [HttpPost]
+        public async Task<List<Image>> AddImageAsync(IFormFile image, [FromQuery] int productId)
+            => await mediator.Send(new AddImage(new ImageStorageDto(image.ToByteArray(), image.GetExtention()), productId));
+
+        [HttpDelete]
+        public async Task DeleteImage(int imageId)
+            => await mediator.Send(new DeleteImage(imageId));
+
+    }
+}
