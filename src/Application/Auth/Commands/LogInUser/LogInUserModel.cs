75251e18fcb23c22a842f428a67ae7e266b5348b
rolfintatu <rolfin.dc@gmail.com>
2021-02-06 10:54:22 +0200

    Added validation for LogInUserModel.

--- src/Application/Auth/Commands/LogInUser/LogInUserModel.cs
+++ src/Application/Auth/Commands/LogInUser/LogInUserModel.cs
@@ -1,4 +1,5 @@
 ﻿using Common.Models;
+using FluentValidation;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,26 @@ namespace Application.Auth.Commands.LogInUser
 {
     public class LogInUserModel : IRequest<TokenModel>
     {
-
         public string Email { get; set; }
         public string Password { get; set; }
         public string Grant_Type { get; set; }
+
+    }
+
+    public class LogInUserModelValidator : AbstractValidator<LogInUserModel>
+    {
+        public LogInUserModelValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .NotNull()
+                .EmailAddress();
+
+            RuleFor(x => x.Password)
+                .NotNull().NotEmpty();
+
+            RuleFor(x => x.Grant_Type)
+                .NotNull().NotEmpty();
+        }
     }
 }
