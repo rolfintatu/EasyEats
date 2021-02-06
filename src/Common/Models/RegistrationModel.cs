0f9d8ef1510c81ef1cdaf81592172a6e705a55f6
rolfintatu <rolfin.dc@gmail.com>
2021-02-06 10:57:50 +0200

    Updated common models.

--- src/Common/Models/RegistrationModel.cs
+++ src/Common/Models/RegistrationModel.cs
@@ -6,21 +6,18 @@ namespace Common.Models
 {
     public class RegistrationModel
     {
-        public RegistrationModel(string id, string email, string password, string name, int phone)
+        public RegistrationModel(string email, string password, string name, string phoneNumber)
         {
-            Id = id;
             Email = email;
             Password = password;
             Name = name;
-            Phone = phone;
+            PhoneNumber = phoneNumber;
         }
-
-        public string Id { get; private set; }
 
         public string Email { get; set; }
         public string Password { get; set; }
 
         public string Name { get; set; }
-        public int Phone { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
