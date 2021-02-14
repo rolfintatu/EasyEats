fee6cab456ce1af42bbc53f8148dcd1d75f4c283
rolfintatu <rolfin.dc@gmail.com>
2021-02-14 10:48:48 +0200

    Added a new value object and base class for value object.

---
+++ src/Domain/ValueObjects/Month.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.ValueObjects
+{
+    public class Month : ValueObject
+    {
+        private Month() { }
+
+        public Month(int monthNumber, int mountDays, int year)
+        {
+            MonthNumber = monthNumber;
+            MountDays = mountDays;
+            Year = year;
+        }
+
+        public int MonthNumber { get; set; }
+        public int MountDays { get; set; }
+        public int Year { get; set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return MonthNumber;
+            yield return MountDays;
+            yield return Year;
+        }
+    }
+}
